efcfa93199576dd9b5df5a4ba0f8db0129c5c922
Sneha <55897319+sneha-afk@users.noreply.github.com>
2024-01-20 17:16:29 -0800

    got up a database connection

---
+++ CITATIONS.md
@@ -0,0 +1,6 @@
+# Citations
+
+These are resources used in this project:
+
+1. ["How to connect a database to a Flask app](https://python-adv-web-apps.readthedocs.io/en/latest/flask_db1.html)
+2. [Quick start on Flask-SQLAlchemy](https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/quickstart/)
--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(
  __name__,
@@ -6,10 +8,34 @@ app = Flask(
  static_folder="../build/static",
 )
 
+# Standard messages
+error_header = "<h1>something went wrong :("
+error_text = "The following error was produced:"
+
+# Creating a database file if it does not exist
+db = SQLAlchemy()
+db_name = 'database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+# Test if database connection is working
+def test_database():
+    try:
+        db.session.query(text("1")).from_statement(text("SELECT 1")).all()
+        return True
+    except Exception as e:
+        return False
+    
+
 @app.route("/")
 def login():
+    if not test_database():
+        return error_header + error_text
+
     return render_template('index.html')
 
+# POST from login
 @app.route("/login", methods=["POST"])
 def handle_login_get():
     if request.method == "POST":
